027105a50de20c2988ea452d807221b3dcb42441
Bryan Rodriguez <bryan_rod20@hotmail.com>
2018-01-17 16:57:45 -0500

    Added nav icons that appear when screen width is less than 768px

--- src/resources/routes/routes.ts
+++ src/resources/routes/routes.ts
@@ -20,11 +20,11 @@ export class Routes
 	private initializeRoutes()
 	{
 		this.routeArray 								= [
-		{ route: ['', 'home'], 	moduleId: 'resources/pages/home/home',			name: 'home', 		title: 'Home'},
-		{ route: 'projects', 	moduleId: 'resources/pages/projects/projects', 	name: 'projects', 	title: 'Projects'},
-		{ route: 'services',	moduleId: 'resources/pages/services/services',	name: 'services',	title: 'Services'},
-		{ route: 'contact',		moduleId: 'resources/pages/contact/contact',	name: 'contact',	title: 'Contact Us'},
-		{ route: 'quotes', 		moduleId: 'resources/pages/quotes/quotes',		name: 'quotes', 	title: 'Quotes'}
+		{ route: ['', 'home'], 	moduleId: 'resources/pages/home/home',			name: 'home', 		title: 'Home', 		 icon: 'fa-home'},
+		{ route: 'projects', 	moduleId: 'resources/pages/projects/projects', 	name: 'projects', 	title: 'Projects', 	 icon: 'fa-folder'},
+		{ route: 'services',	moduleId: 'resources/pages/services/services',	name: 'services',	title: 'Services', 	 icon: 'fa-code'},
+		{ route: 'contact',		moduleId: 'resources/pages/contact/contact',	name: 'contact',	title: 'Contact Us', icon: 'fa-phone'},
+		{ route: 'quotes', 		moduleId: 'resources/pages/quotes/quotes',		name: 'quotes', 	title: 'Quotes', 	 icon: 'fa-dollar'}
 		];
 	}
 
